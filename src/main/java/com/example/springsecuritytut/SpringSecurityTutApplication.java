eed1f3d2cffe56f30d15241775003bdfcb50b6c7
Rafal Gostynski <rgostynski@gmail.com>
2021-08-24 20:34:29 +1200

    Initial Commit

---
+++ src/main/java/com/example/springsecuritytut/SpringSecurityTutApplication.java
@@ -0,0 +1,13 @@
+package com.example.springsecuritytut;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityTutApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityTutApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/springsecuritytut/student/StudentController.java
@@ -0,0 +1,4 @@
+package com.example.springsecuritytut.student;
+
+public class StudentController {
+}
