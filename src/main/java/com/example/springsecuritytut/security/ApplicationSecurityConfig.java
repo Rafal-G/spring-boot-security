1274075c5c69e2f13152549ffef3c17ff78564eb
Rafal Gostynski <rgostynski@gmail.com>
2021-08-25 07:11:39 +1200

    user detail service to hardcode a user, won't work currently because the password is not encoded.

--- src/main/java/com/example/springsecuritytut/security/ApplicationSecurityConfig.java
+++ src/main/java/com/example/springsecuritytut/security/ApplicationSecurityConfig.java
@@ -1,9 +1,14 @@
 package com.example.springsecuritytut.security;
 
 import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configurable
 @EnableWebSecurity
@@ -18,5 +23,18 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
             .and()
             .httpBasic();
 
+    }
+
+    @Override
+    @Bean
+    // How to retrieve user from database
+    protected UserDetailsService userDetailsService() {
+        UserDetails user1 = User.builder()
+                .username("user1")
+                .password("password")
+                .roles("STUDENT")
+                .build();
+
+        return new InMemoryUserDetailsManager(user1);
     }
 }
