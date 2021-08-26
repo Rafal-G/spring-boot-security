adc6e7eb6174f33ab2a001433399f97553361c3e
Rafal Gostynski <rgostynski@gmail.com>
2021-08-27 07:03:17 +1200

    New ADMIN_TRAINEE role

--- src/main/java/com/example/springsecuritytut/security/ApplicationUserRole.java
+++ src/main/java/com/example/springsecuritytut/security/ApplicationUserRole.java
@@ -8,10 +8,10 @@ import static com.example.springsecuritytut.security.ApplicationUserPermission.*
 
 public enum ApplicationUserRole {
     STUDENT(Sets.newHashSet()),
-    ADMIN(Sets.newHashSet(COURSES_READ, COURSES_WRITE, STUDENT_READ, STUDENT_WRITE));
+    ADMIN(Sets.newHashSet(COURSES_READ, COURSES_WRITE, STUDENT_READ, STUDENT_WRITE)),
+    ADMIN_TRAINEE(Sets.newHashSet(COURSES_READ, STUDENT_READ));
 
     private final Set<ApplicationUserPermission> permissions;
-
 
     ApplicationUserRole(Set<ApplicationUserPermission> permissions) {
         this.permissions = permissions;
