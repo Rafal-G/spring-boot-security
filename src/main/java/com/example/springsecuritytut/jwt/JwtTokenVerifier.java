608f03305edaf9fe6f5a1ee39747ba39f188881d
Rafal Gostynski <rgostynski@gmail.com>
2021-08-29 14:39:48 +1200

    JwtTokenVerifier to verify JWT tokens.

---
+++ src/main/java/com/example/springsecuritytut/jwt/JwtTokenVerifier.java
@@ -0,0 +1,66 @@
+package com.example.springsecuritytut.jwt;
+
+import com.google.common.base.Strings;
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class JwtTokenVerifier extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain) throws ServletException, IOException {
+
+        String authorizationHeader = request.getHeader("Authorization");
+
+        if (Strings.isNullOrEmpty(authorizationHeader) || !authorizationHeader.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        String token = authorizationHeader.replace("Bearer ", "");
+        try {
+            String secretKey = "supersecuresupersecuresupersecuresupersecuresupersecuresupersecure";
+
+            //  Jwts.parser()
+            //        .setSigningKey(secretKey)
+            //        .parseClaimsJws(token)
+            JwtParser parser = Jwts.parserBuilder().setSigningKey(Keys.hmacShaKeyFor(secretKey.getBytes())).build();
+            Jws<Claims> claimsJws = parser.parseClaimsJws(token);
+            Claims body = claimsJws.getBody();
+            String userName = body.getSubject();
+
+            var authorities = (List<Map<String, String>>) body.get("authorities");
+            Set<SimpleGrantedAuthority> simpleGrantedAuthorities = authorities.stream()
+                    .map(m -> new SimpleGrantedAuthority(m.get("authority")))
+                    .collect(Collectors.toSet());
+
+            Authentication authentication = new UsernamePasswordAuthenticationToken(
+                    userName,
+                    null,
+                    simpleGrantedAuthorities
+            );
+
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+
+
+        } catch (JwtException e) {
+            throw new IllegalStateException("token cannot be verified " + token);
+        }
+
+    }
+}
